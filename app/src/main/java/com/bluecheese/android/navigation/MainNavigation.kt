877d8ac140ae336d65abf5e667bb2df2bf5c2a69
Massimo Petrogalli <>
2024-03-19 23:46:37 +0100

    feat: added animations to navigation

--- app/src/main/java/com/bluecheese/android/navigation/MainNavigation.kt
+++ app/src/main/java/com/bluecheese/android/navigation/MainNavigation.kt
@@ -1,6 +1,12 @@
 package com.bluecheese.android.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import com.bluecheese.android.presentation.login.LoginScreen
 import com.bluecheese.android.presentation.login.LoginViewModel
@@ -8,7 +14,35 @@ import com.bluecheese.android.presentation.login.LoginViewModel
 @Composable
 fun MainNavigation(navigationController: Router, startDestination: NavigationParameter) {
     val navController = navigationController.getNavController()
-    NavHost(navController = navController, startDestination = startDestination.id) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination.id,
+        modifier = Modifier.fillMaxSize(),
+        enterTransition = {
+            slideIntoContainer(
+                AnimatedContentTransitionScope.SlideDirection.Start,
+                tween(500)
+            )
+        },
+        exitTransition = {
+            slideOutOfContainer(
+                AnimatedContentTransitionScope.SlideDirection.Start,
+                tween(500)
+            )
+        },
+        popEnterTransition = {
+            slideIntoContainer(
+                AnimatedContentTransitionScope.SlideDirection.End,
+                tween(500)
+            )
+        },
+        popExitTransition = {
+            slideOutOfContainer(
+                AnimatedContentTransitionScope.SlideDirection.End,
+                tween(500)
+            )
+        },
+    ) {
         composable<LoginViewModel>(
             route = NavigationParameter.Login,
             navController = navController
