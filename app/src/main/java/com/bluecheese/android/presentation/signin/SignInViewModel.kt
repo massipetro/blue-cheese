5c23f8054dae9eee56019968f587a6e46716a289
Massimo Petrogalli <>
2024-04-19 22:39:37 +0200

    feat: added signup logics

--- app/src/main/java/com/bluecheese/android/presentation/signin/SignInViewModel.kt
+++ app/src/main/java/com/bluecheese/android/presentation/signin/SignInViewModel.kt
@@ -1,19 +1,22 @@
 package com.bluecheese.android.presentation.signin
 
 import android.app.Activity
+import android.content.Context
 import android.util.Log
 import com.bluecheese.android.navigation.NavigationParameter
 import com.bluecheese.android.navigation.RouterImpl
 import com.bluecheese.android.navigation.navigateTo
 import com.bluecheese.android.presentation.common.RoutingViewModel
+import com.bluecheese.android.utils.message
 import com.bluecheese.domain.integrations.GoogleAuthClient
 import com.bluecheese.domain.interactor.LoginUseCase
 import com.bluecheese.domain.interactor.SignUpUseCase
-import com.bluecheese.mvi.extension.concatReducers
+import com.bluecheese.mvi.dsl.updateState
 import com.bluecheese.mvi.foundation.Model
 import com.bluecheese.mvi.foundation.Store
 import com.bluecheese.mvi.viewmodel.stateMachine
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
@@ -23,6 +26,7 @@ import javax.inject.Inject
 @HiltViewModel
 class SignInViewModel @Inject constructor(
     router: RouterImpl,
+    @ApplicationContext private val context: Context,
     private val store: Store<SignInState>,
     private val reducers: SignInReducers,
     private val loginUseCase: LoginUseCase,
@@ -36,31 +40,32 @@ class SignInViewModel @Inject constructor(
     ) {
         on<SignInIntent.ChangeEmail>() updateState reducers::changeEmail
         on<SignInIntent.ChangePassword>() updateState reducers::changePassword
+        on<SignInIntent.ChangeConfirmPassword>() updateState reducers::changeConfirmPassword
 
         on<SignInIntent.Login>() sideEffect {
             LoginUseCase.Params(currentState.email, currentState.password)
-                .let { loginUseCase.perform(it) }
+                .let(loginUseCase::perform)
                 .onStart {
-                    concatReducers(
+                    updateState(
                         reducers.resetLoginError,
                         reducers.startLoginLoading
-                    ).let(::updateState)
+                    )
                 }
                 .onEach { result ->
                     result
                         .mapLeft {
                             Log.w("Login", "SignIn Failure: ${it.message}")
-                            concatReducers(
+                            updateState(
                                 reducers.setLoginFailed,
                                 reducers.stopLoginLoading
-                            ).let(::updateState)
+                            )
                         }
                         .map {
                             Log.d("Login", "SignIn Successful: ${currentState.email}")
-                            concatReducers(
+                            updateState(
                                 reducers.stopLoginLoading,
                                 reducers.resetLoginError
-                            ).let(::updateState)
+                            )
                             // TODO create saveCurrentUser UseCase
                             navigateTo(NavigationParameter.Home)
                         }
@@ -84,15 +89,45 @@ class SignInViewModel @Inject constructor(
         }
 
         on<SignInIntent.CreateAccount>() sideEffect {
-            concatReducers(
+            updateState(
                 reducers.changeEmail(SignInIntent.ChangeEmail("")),
                 reducers.changePassword(SignInIntent.ChangePassword("")),
-            ).let(::updateState)
+            )
             navigateTo(NavigationParameter.SignUp)
         }
 
         on<SignInIntent.Signup>() sideEffect {
-
+            val state = currentState
+            // TODO add check if confirmPassword and password are the same
+            SignUpUseCase.Params(state.email, state.confirmPassword)
+                .let(signUpUseCase::perform)
+                .onStart {
+                    updateState(
+                        reducers.updateSignUpError(null),
+                        reducers.startLoginLoading
+                    )
+                }
+                .onEach { result ->
+                    result
+                        .mapLeft {
+                            Log.w("SignUp", "SignUp Failure: ${it.message}")
+                            val exception = it.message(context) ?: it.localizedMessage
+                            updateState(
+                                reducers.updateSignUpError(exception),
+                                reducers.stopLoginLoading
+                            )
+                        }
+                        .map {
+                            Log.d("SignUp", "SignUp Successful: ${currentState.email}")
+                            updateState(
+                                reducers.stopLoginLoading,
+                                reducers.updateSignUpError(null)
+                            )
+                            // TODO create saveCurrentUser UseCase
+                            navigateTo(NavigationParameter.Home)
+                        }
+                }
+                .collect()
         }
     }
 }
